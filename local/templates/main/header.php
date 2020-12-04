24489ff5b8971ce8903157793aa8f273e75d259e
cesnakas <10352420+cesnakas@users.noreply.github.com>
2020-12-04 21:40:36 +0400

    1.0.1

---
+++ local/templates/main/header.php
@@ -0,0 +1,26 @@
+<?if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();?>
+<!DOCTYPE html>
+<html lang="<?=LANGUAGE_ID;?>">
+<head>
+
+<?
+    $APPLICATION->ShowHead();
+    // Bitrix
+    use Bitrix\Main\Page\Asset;
+    // Meta
+    Asset::getInstance()->addString('<meta name="viewport" content="width=device-width, initial-scale=1"/>');
+    // CSS
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH.'/css/swiper-bundle.min.css');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH.'/css/style.css');
+    // JS
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH.'/js/swiper-bundle.min.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH.'/js/main.js');
+?>
+
+    <title><?$APPLICATION->ShowTitle();?></title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+
+</head>
+<div id="panel"><?$APPLICATION->ShowPanel();?></div>
+<body>

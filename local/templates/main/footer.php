dcd74df08049407b9a00b9a5763894e1f1058712
cesnakas <10352420+cesnakas@users.noreply.github.com>
2020-12-09 18:23:34 +0400

    1.1.11

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -1,5 +1,7 @@
 <?if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();?>
 
+        </div>
+
         <footer>
             <div class="container">
                 <div class="footer-top">

c318ea262f355a242bfd2a61afe9b5c02e662b77
cesnakas <10352420+cesnakas@users.noreply.github.com>
2020-12-11 14:31:28 +0400

    1.4.19

--- catalog/index.php
+++ catalog/index.php
@@ -27,33 +27,33 @@ $APPLICATION->SetTitle("Каталог");
         </div>
     </div>
 
-    <div class="container-banner">
+    <!--<div class="container-banner">
         <div class="banner-ad">
             <div class="swiper-container slider-banner-ad">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide">
                         <a href="#">
-                            <img src="<?=SITE_TEMPLATE_PATH;?>/img/banner/banner1.png" alt="">
+                            <img src="<?/*=SITE_TEMPLATE_PATH;*/?>/img/banner/banner1.png" alt="">
                         </a>
                     </div>
                     <div class="swiper-slide">
                         <a href="#">
-                            <img src="<?=SITE_TEMPLATE_PATH;?>/img/banner/banner1.png" alt="">
+                            <img src="<?/*=SITE_TEMPLATE_PATH;*/?>/img/banner/banner1.png" alt="">
                         </a>
                     </div>
                     <div class="swiper-slide">
                         <a href="#">
-                            <img src="<?=SITE_TEMPLATE_PATH;?>/img/banner/banner1.png" alt="">
+                            <img src="<?/*=SITE_TEMPLATE_PATH;*/?>/img/banner/banner1.png" alt="">
                         </a>
                     </div>
                     <div class="swiper-slide">
                         <a href="#">
-                            <img src="<?=SITE_TEMPLATE_PATH;?>/img/banner/banner1.png" alt="">
+                            <img src="<?/*=SITE_TEMPLATE_PATH;*/?>/img/banner/banner1.png" alt="">
                         </a>
                     </div>
                     <div class="swiper-slide">
                         <a href="#">
-                            <img src="<?=SITE_TEMPLATE_PATH;?>/img/banner/banner1.png" alt="">
+                            <img src="<?/*=SITE_TEMPLATE_PATH;*/?>/img/banner/banner1.png" alt="">
                         </a>
                     </div>
                 </div>
@@ -62,7 +62,71 @@ $APPLICATION->SetTitle("Каталог");
                 <div class="swiper-button-next banner-button-next"></div>
             </div>
         </div>
-    </div>
+    </div>-->
+
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:news.list",
+        ".default",
+        Array(
+            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+            "ADD_SECTIONS_CHAIN" => "Y",
+            "AJAX_MODE" => "N",
+            "AJAX_OPTION_ADDITIONAL" => "",
+            "AJAX_OPTION_HISTORY" => "N",
+            "AJAX_OPTION_JUMP" => "N",
+            "AJAX_OPTION_STYLE" => "Y",
+            "CACHE_FILTER" => "N",
+            "CACHE_GROUPS" => "Y",
+            "CACHE_TIME" => "36000000",
+            "CACHE_TYPE" => "A",
+            "CHECK_DATES" => "Y",
+            "DETAIL_URL" => "",
+            "DISPLAY_BOTTOM_PAGER" => "Y",
+            "DISPLAY_DATE" => "Y",
+            "DISPLAY_NAME" => "Y",
+            "DISPLAY_PICTURE" => "Y",
+            "DISPLAY_PREVIEW_TEXT" => "Y",
+            "DISPLAY_TOP_PAGER" => "N",
+            "FIELD_CODE" => array("", ""),
+            "FILTER_NAME" => "",
+            "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+            "IBLOCK_ID" => "2",
+            "IBLOCK_TYPE" => "news",
+            "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+            "INCLUDE_SUBSECTIONS" => "Y",
+            "MEDIA_PROPERTY" => "",
+            "MESSAGE_404" => "",
+            "NEWS_COUNT" => "5",
+            "PAGER_BASE_LINK_ENABLE" => "N",
+            "PAGER_DESC_NUMBERING" => "N",
+            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+            "PAGER_SHOW_ALL" => "N",
+            "PAGER_SHOW_ALWAYS" => "N",
+            "PAGER_TEMPLATE" => ".default",
+            "PAGER_TITLE" => "Новости",
+            "PARENT_SECTION" => "",
+            "PARENT_SECTION_CODE" => "",
+            "PREVIEW_TRUNCATE_LEN" => "",
+            "PROPERTY_CODE" => array("", ""),
+            "SEARCH_PAGE" => "/search/",
+            "SET_BROWSER_TITLE" => "Y",
+            "SET_LAST_MODIFIED" => "N",
+            "SET_META_DESCRIPTION" => "Y",
+            "SET_META_KEYWORDS" => "Y",
+            "SET_STATUS_404" => "N",
+            "SET_TITLE" => "Y",
+            "SHOW_404" => "N",
+            "SLIDER_PROPERTY" => "",
+            "SORT_BY1" => "ACTIVE_FROM",
+            "SORT_BY2" => "SORT",
+            "SORT_ORDER1" => "DESC",
+            "SORT_ORDER2" => "ASC",
+            "STRICT_SECTION_CHECK" => "N",
+            "TEMPLATE_THEME" => "blue",
+            "USE_RATING" => "N",
+            "USE_SHARE" => "N"
+        )
+    );?>
 
     <div class="container">
 
--- local/templates/main/components/bitrix/breadcrumb/.default/template.php
+++ local/templates/main/components/bitrix/breadcrumb/.default/template.php
@@ -13,25 +13,14 @@ if(empty($arResult))
 
 $strReturn = '';
 
-//we can't use $APPLICATION->SetAdditionalCSS() here because we are inside the buffered function GetNavChain()
-/*
-$css = $APPLICATION->GetCSSArray();
-if(!is_array($css) || !in_array("/bitrix/css/main/font-awesome.css", $css))
-{
-	$strReturn .= '<link href="'.CUtil::GetAdditionalFileURL("/bitrix/css/main/font-awesome.css").'" type="text/css" rel="stylesheet" />'."\n";
-}
-*/
-
 $strReturn .= '<ul class="header-breadcrumb__list" itemprop="http://schema.org/breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">';
 
 $itemSize = count($arResult);
-for($index = 0; $index < $itemSize; $index++)
-{
+for($index = 0; $index < $itemSize; $index++) {
 	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
 	/* $arrow = ($index > 0? '<i class="fa fa-angle-right"></i>' : ''); */
 
-	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
-	{
+	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1) {
 		$strReturn .= '
 			<li class="header-breadcrumb__item" id="bx_breadcrumb_'.$index.'" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
 				'.$arrow.'
@@ -40,9 +29,7 @@ for($index = 0; $index < $itemSize; $index++)
 				</a>
 				<meta itemprop="position" content="'.($index + 1).'" />
 			</li>';
-	}
-	else
-	{
+	} else {
 		$strReturn .= '
 			<li class="header-breadcrumb__item">
 				'.$arrow.'
